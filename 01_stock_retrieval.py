cf3dca058380b2d857aa6c1fa9989edc6283ac6f
Mliam201 <morrow.liam@gmail.com>
2020-05-27 10:18:35 -0400

    Update both notebook and py versions

--- 01_stock_retrieval.py
+++ 01_stock_retrieval.py
@@ -1,8 +1,9 @@
 #required imports
 import pandas as pd
 import pandas_datareader as wb
+
 #function to pull multiple stocks into several dataframes from yahoo.
-def stock_retrieval(tickers,start_date,end_date):
+def stock_fetch(tickers,start_date,end_date):
     #remove commas
     #should add in a check for unique, remove spaces, and check to see if these are needed
     tickers = tickers.split(',')
@@ -13,16 +14,15 @@ def stock_retrieval(tickers,start_date,end_date):
         d[tickers] = wb.DataReader(tickers, data_source='yahoo', start=(start_date), end=(end_date))
     return d
 
-
-#required input
+#Sample Required Input
 tickers = 'PG,MSFT,F,GE'
 start_date = '2012-01-01'
 end_date = '2017-01-01'
 
 #call for stock information.
-stock_df = stock_retrieval(tickers, start_date, end_date)
+stock_df = stock_fetch(tickers, start_date, end_date)
 #stock list
-stock_df[1]
+stock_df.keys()
 #dataframes
-stock_df[0]
-tickers
+for t in stock_df.keys():
+    print(stock_df[t])
