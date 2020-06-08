c1406e97dd10a3a391bef858fa9907b72e58f4f3
Mliam201 <64180127+Mliam201@users.noreply.github.com>
2020-06-07 23:42:41 -0400

    Added 2 modules for efficient frontier.

--- Python_Core/portfolio_optimization_risk.py
+++ Python_Core/portfolio_optimization_risk.py
@@ -1,53 +1,50 @@
 # %% markdown
-# # Portfolio Optimization - Risk
+# Portfolio Optimization - Risk
 # %% imports
 import numpy as np
 import pandas as pd
 from pandas_datareader import data as wb
 import matplotlib.pyplot as plt
+import seaborn as sns
 from datetime import datetime
 from pandas import ExcelWriter
 from pandas import ExcelFile
+from scipy.optimize import minimize
+from stock_fetch import stock_utilities as sf
+from pypfopt.efficient_frontier import EfficientFrontier
+from pypfopt.expected_returns import mean_historical_return
+from pypfopt.risk_models import CovarianceShrinkage
+from pypfopt import Plotting
+import pypfopt
+sns.set(style="darkgrid")
+%matplotlib inline
 
-if __name__ == '__main__' and __package__ is None:
-    import sys, os.path
-    sys.path
-    # append parent of the directory the current file is in
-    inputfilename = r"C:\Users\Liam Morrow\Documents\Onedrive\Python scripts\_01 Liam Stock Analysis Project\stock_analysis\Python_Core"
-    inputfilename = r"C:\Users\Liam Morrow\.conda\pkgs"
-    inputfilename = r"C:\Users\Liam Morrow\anaconda3"
+# if __name__ == '__main__' and __package__ is None:
+#     import sys, os.path
+#     sys.path
+#     # append parent of the directory the current file is in
+#     inputfilename1 = r"C:\Users\Liam Morrow\Documents\Onedrive\Python scripts\_01 Liam Stock Analysis Project\stock_analysis\Python_Core"
+#     inputfilename2 = r"C:\Users\l.morrow\OneDrive\Python scripts\_01 Liam Stock Analysis Project\stock_analysis\Python_Core"
+#     #inputfilename = r"C:\Users\Liam Morrow\.conda\pkgs"
+#     #inputfilename = r"C:\Users\Liam Morrow\anaconda3"
+#     sys.path.append(inputfilename1)
+#     sys.path.append(inputfilename2)
 
-    sys.path.append(os.path.dirname(os.path.abspath(inputfilename)))
-    sys.path.append(inputfilename)
+# %% fetch stock data
+# tickers="AFDIX,FXAIX,JLGRX,MEIKX,PGOYX,FSMDX,HFMVX,FCVIX,FSSNX,WSCGX,CVMIX,DOMOX,FSPSX,ODVYX,MINJX,FGDIX,CMJIX,FFIVX,FCIFX,FFVIX,FDIFX,FIAFX,BPRIX,CBDIX,OIBYX,PDBZX"
+tickers="AFDIX,FXAIX,JLGRX,MEIKX"
+start_date = datetime(2015,1,1)
+end_date = datetime(2020,6,1)
+stock_df = sf.yahoo_stock_fetch(tickers, start_date, end_date)
+
+# %% Excel Functions
+# sf.write_to_excel(stock_df)
+# new_df = sf.read_from_excel(r"C:\Users\Liam Morrow\Documents\Onedrive\Python scripts\_01 Liam Stock Analysis Project\stock_analysis\Python_Core\funds.xlsx")
+
 
 
 # %% fetch stock data
-from stock_fetch import stock_fetch as sf
-#tickers = 'PG,MSFT,F,GE'
-#tickers = 'FSPSX,FSSNX,FXAIX,JLGRX'
-tickers="AFDIX,FXAIX,JLGRX,MEIKX,PGOYX,FSMDX,HFMVX,FCVIX,FSSNX,WSCGX,CVMIX,DOMOX,FSPSX,ODVYX,MINJX,FGDIX,CMJIX,FFIVX,FCIFX,FFVIX,FDIFX,FIAFX,BPRIX,CBDIX,OIBYX,PDBZX"
 
-start_date = datetime(2010,1,1)
-end_date = datetime(2020,6,1)
-stock_df = sf.yahoo_stock_fetch(tickers, start_date, end_date)
-# %% Pull stock portfolio weights
-# need to code into the stock fetch module
-#excel pull and try to get an etrade pull
-
-# %% print to excel
-excel_name = pd.ExcelWriter('funds.xlsx', engine = 'xlsxwriter')
-print('Saving:')
-for i in stock_df.keys():
-    print (i)
-    stock_df[i].to_excel(excel_name,str(i))
-excel_name.save()
-
-# %% Read from excel
-all_sheets_df = pd.read_excel('funds.xlsx', sheet_name=None)
-for i in all_sheets_df.keys():
-    all_sheets_df[i].set_index('Date', inplace=True)
-
-# %% compile returns
 analysis_df = {}
 for t in stock_df.keys():
     analysis_df[t] = pd.DataFrame()
@@ -56,159 +53,183 @@ for t in stock_df.keys():
     analysis_df[t]['Total ROI %'] = ((stock_df[t]['Adj Close']-stock_df[t]['Adj Close'].iloc[0])/stock_df[t]['Adj Close'].iloc[0])*100
     analysis_df[t]['Log Returns'] = np.log(stock_df[t]['Adj Close']/stock_df[t]['Adj Close'].shift(1))
 
-
-# %% table plot
-plt.figure(figsize=(16,8))
+adj_close_df = pd.DataFrame()
 for t in stock_df.keys():
-    plt.plot(analysis_df[t]['Total ROI %'])
-plt.ylabel('Log Return')
-plt.xlabel('Year')
-plt.title('Log Return')
-plt.legend(stock_df.keys())
+    adj_close_df[t] = analysis_df[t]['Adj Close']
+adj_close_df
+
+
+# %% Efficient Frontier
+
+class efficient_frontier_models: 
+
+    def pyfolio_eff_frontier(adj_close_df,cov_type = "ledoit_wolf",returns = False,risk_free_rate=0.02):
+        """Use pyfolio to generate a efficient frontier. Comes with less control over the plot. Returns values to use for analysis.
+
+        Args:
+            adj_close_df ([DataFrame]): A dataframe of either adj close prices or returns
+            cov_type (str, optional): Type of covariance to consider. Inputs are "simple" for a simple covariance calulation and "ledoit_wolf" for taking shrinkage into account. Defaults to "ledoit_wolf".
+            returns (bool, optional): True for returns False for adjusted close prices. Defaults to False.
+            risk_free_rate (float, optional): Risk Free rate modifier. Defaults to 0.02.
+
+        Returns:
+            [tuple]: [0] weights of the max sharpe value [1] mean historical return [2] Covariance matrix
+        """
+        mu = mean_historical_return(adj_close_df, returns_data=returns)
+
+        if cov_type == "simple":
+            S = pypfopt.risk_models.sample_cov(adj_close_df, returns_data=returns, frequency=252)
+            
+        elif cov_type == "ledoit_wolf":
+            S = CovarianceShrinkage(adj_close_df, returns_data=returns).ledoit_wolf()
+        else:
+            S = CovarianceShrinkage(adj_close_df, returns_data=returns).ledoit_wolf()
+
+        ef = EfficientFrontier(mu, S)
+        weights = ef.max_sharpe(risk_free_rate=risk_free_rate)
+        cleaned_weights = ef.clean_weights()
+        
+        #Turn off deprecated warnings
+        import warnings
+
+        def fxn():
+            warnings.warn("deprecated", DeprecationWarning)
+
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+        fxn()
+        return cleaned_weights,mu,S
+
+
+    def monte_carlo_eff_frontier(adj_close_df, n_portfolios = 1000, trading_days = 252, seed = 1, n_points_on_curve = 100):
+        """Use Monte Carlo approach to generate a efficient frontier. Returns values to use for analysis.
+
+        Args:
+            adj_close_df ([DataFrame]): A dataframe of adj close prices 
+            n_portfolios (int, optional): Number of random portfolios to generate. Defaults to 1000.
+            trading_days (int, optional): Number of assumed trading days. Defaults to 252.
+            seed (int, optional): Random seed. Defaults to 1.
+            n_points_on_curve (int, optional): Number of points on the eff frontier curve. Defaults to 100.
+
+        Returns:
+            [tuple]: [0] weights of all portfolios, [1] results for the optimal portfolios along the efficient frontier, [2] results of the random portfolios including returns, volatility, and sharpe ratio, [3]stock tickers, [4] covariance matrix of all the stocks analyzed
+        """
+        #annualized average returns and the corresponding standard deviation
+        returns_df = adj_close_df.pct_change().dropna()
+        avg_returns = returns_df.mean() * trading_days
+        cov_mat = returns_df.cov() * trading_days
+        tickers = returns_df.keys()
+
+        #random weights for portfolios
+        np.random.seed(seed)
+        weights = np.random.random(size=(n_portfolios, len(returns_df.keys())))
+        weights /=  np.sum(weights, axis=1)[:, np.newaxis]
+
+        #random portfolio metrics
+        portf_rtns = np.dot(weights, avg_returns)
+        portf_vol = []
+        for i in range(0, len(weights)):
+            portf_vol.append(np.sqrt(np.dot(weights[i].T, 
+                                            np.dot(cov_mat, weights[i]))))
+        portf_vol = np.array(portf_vol)  
+        portf_sharpe_ratio = portf_rtns / portf_vol
+
+        #df for random portfolios
+        portf_results_df = pd.DataFrame({'returns': portf_rtns,
+                                        'volatility': portf_vol,
+                                        'sharpe_ratio': portf_sharpe_ratio})
+
+        #locate points to plot efficient frontier
+        portf_vol_ef = []
+        indices_to_skip = []
+
+        portf_rtns_ef = np.linspace(portf_results_df.returns.min(), 
+                                    portf_results_df.returns.max(), 
+                                    n_points_on_curve)
+        portf_rtns_ef = np.round(portf_rtns_ef, 2)    
+        portf_rtns = np.round(portf_rtns, 2)
+
+        for point_index in range(n_points_on_curve):
+            if portf_rtns_ef[point_index] not in portf_rtns:
+                indices_to_skip.append(point_index)
+                continue
+            matched_ind = np.where(portf_rtns == portf_rtns_ef[point_index])
+            portf_vol_ef.append(np.min(portf_vol[matched_ind]))
+            
+        portf_rtns_ef = np.delete(portf_rtns_ef, indices_to_skip)
+        plot_results_df = pd.DataFrame({'returns': portf_rtns_ef,
+                                        'volatility': portf_vol_ef})
+        
+
+        #print the max sharpe portfolio and min volatility portfolio
+        max_sharpe_ind = np.argmax(portf_results_df.sharpe_ratio)
+        max_sharpe_portf = portf_results_df.loc[max_sharpe_ind]
+        min_vol_ind = np.argmin(portf_results_df.volatility)
+        min_vol_portf = portf_results_df.loc[min_vol_ind]
+
+        print('Maximum Sharpe Ratio Portfolio ----')
+        print('Performance')
+        #!!! Sharpe is a ratio not a percent. needs fixed below.
+        for index, value in max_sharpe_portf.items():
+            print(f'{index}: {100 * value:.2f}%   ', end="", flush=True)
+        print('\nWeights')
+        for x, y in zip(returns_df.keys(),
+        weights[max_sharpe_ind]):
+            print(f'{x}: {100*y:.2f}%   ', end="", flush=True)
+
+        print('\n\nMinimum Volatility Portfolio ----')
+        print('Performance')
+        for index, value in min_vol_portf.items():
+            print(f'{index}: {100 * value:.2f}%   ', end=" ", flush=True)
+        print('\nWeights')
+        for x, y in zip(returns_df.keys(),
+        weights[min_vol_ind]):
+            print(f'{x}: {100*y:.2f}%   ', end="", flush=True)
+
+
+        return weights, plot_results_df, portf_results_df, tickers, cov_mat, max_sharpe_portf, min_vol_portf,avg_returns
+        
+
+
+# %% Monte Carlo Run
+monte_carlo_ef = efficient_frontier_models.monte_carlo_eff_frontier(adj_close_df,n_portfolios = 3000,n_points_on_curve=100)
+
+
+# %% Monte Carlo Plot
+fig, ax = plt.subplots(figsize=(12,10))
+#plot the random portfolios
+monte_carlo_ef[2].plot(kind='scatter', x='volatility', 
+                      y='returns', c='sharpe_ratio',
+                      cmap='RdYlGn', edgecolors='black', 
+                      ax=ax)
+ax.set(xlabel='Volatility', 
+       ylabel='Expected Returns', 
+       title='Efficient Frontier')
+#plot the efficient frontier line
+ax.plot(monte_carlo_ef[1]['volatility'], monte_carlo_ef[1]['returns'], 'b--')
+for asset_index in range(len(monte_carlo_ef[3])):
+    ax.scatter(x=np.sqrt(monte_carlo_ef[4].iloc[asset_index, asset_index]),y=monte_carlo_ef[7][asset_index],marker='o',s=150,color='black',edgecolors='red',label=monte_carlo_ef[3][asset_index])
+    ax.annotate((monte_carlo_ef[3][asset_index]), (np.sqrt(monte_carlo_ef[4].iloc[asset_index, asset_index]), monte_carlo_ef[7][asset_index]), xytext=(10,10), textcoords='offset points')
+
+
+ax.scatter(x=monte_carlo_ef[5].volatility, y=monte_carlo_ef[5].returns, c='black', marker='*', s=200, label='Max Sharpe Ratio')
+ax.scatter(x=monte_carlo_ef[6].volatility,y=monte_carlo_ef[6].returns,c='black',marker='P',s=200, label='Minimum Volatility')
+ax.legend()
 plt.tight_layout()
 plt.show()
 
-# %% Portfolio Test Weights
-num_assets = len(stock_df.keys())
-num_ports = 1000
-# create random numberes that equal 1. these will be used to test the different percentages of the portfolios
-
-pfolio_returns = []
-pfolio_volatilities = []
-log_ret_df = pd.DataFrame()
-
-all_weights = np.zeros((num_ports,num_assets))
-ret_arr = np.zeros(num_ports)
-vol_arr = np.zeros(num_ports)
-sharpe_arr = np.zeros(num_ports)
-
-for t in stock_df.keys():
-    log_ret_df[t] = analysis_df[t]['Log Returns']
-
-for x in range(num_ports):
-    weights = np.random.random(num_assets)
-    weights /= np.sum(weights)
-    all_weights[x,:] = weights
-    # Expected Return
-    ret_arr[x] = np.sum((log_ret_df.mean() * weights) *252)
-
-    # Expected Variance
-    vol_arr[x] = np.sqrt(np.dot(weights.T, np.dot(log_ret_df.cov() * 252, weights)))
-
-    # Sharpe Ratio
-    sharpe_arr[x] = ret_arr[x]/vol_arr[x]
-
-max_sr_ret = ret_arr[sharpe_arr.argmax()]
-max_sr_vol = vol_arr[sharpe_arr.argmax()]
-
-plt.figure(figsize=(12,8))
-plt.scatter(vol_arr,ret_arr,c=sharpe_arr,cmap='plasma')
-plt.colorbar(label='Sharpe Ratio')
-plt.xlabel('Volatility')
-plt.ylabel('Return')
-
-# Add red dot for max SR
-eff_frontier = plt.scatter(max_sr_vol,max_sr_ret,c='red',s=50,edgecolors='black')
 
 
-# %% Find Covariance and Correllation
-# Stock Columns
-print('Stocks: ')
-print(stock_df.keys())
-print('\n')
-
-# Rebalance Weights
-print('Weights:')
-print((all_weights[sharpe_arr.argmax(),:])*100)
-print('\n')
-
-# Expected Return
-print('Expected Portfolio Return: ')
-exp_ret = (np.sum(log_ret_df.mean() * weights) *252)*100
-print("%.2f%%" % exp_ret)
-print('\n')
-
-# Expected Variance
-print('Expected Volatility: ')
-exp_vol = (np.sqrt(np.dot(weights.T, np.dot(log_ret_df.cov() * 252, weights))))*100
-print("%.2f%%" % exp_vol)
-print('\n')
-
-# Sharpe Ratio
-print('Sharpe Ratio: ')
-print(sharpe_arr.max())
-# %% codecell
-log_ret_df
-def get_ret_vol_sr(weights):
-    """
-    Takes in weights, returns array or return,volatility, sharpe ratio
-    """
-    weights = np.array(weights)
-    ret = np.sum(log_ret.mean() * weights) * 252
-    vol = np.sqrt(np.dot(weights.T, np.dot(log_ret.cov() * 252, weights)))
-    sr = ret/vol
-    return np.array([ret,vol,sr])
-# %% codecell
- %% markdown
-# ### Functionalize Return and SR operations
-# %% codecell
-def get_ret_vol_sr(weights):
-    """
-    Takes in weights, returns array or return,volatility, sharpe ratio
-    """
-    weights = np.array(weights)
-    ret = np.sum(log_ret.mean() * weights) * 252
-    vol = np.sqrt(np.dot(weights.T, np.dot(log_ret.cov() * 252, weights)))
-    sr = ret/vol
-    return np.array([ret,vol,sr])
-# %% codecell
-from scipy.optimize import minimize
-# %% markdown
-# To fully understand all the parameters, check out:
-# https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html
-# %% codecell
-help(minimize)
-# %% markdown
-# Optimization works as a minimization function, since we actually want to maximize the Sharpe Ratio, we will need to turn it negative so we can minimize the negative sharpe (same as maximizing the postive sharpe)
-# %% codecell
-def neg_sharpe(weights):
-    return  get_ret_vol_sr(weights)[2] * -1
-# %% codecell
-# Contraints
-def check_sum(weights):
-    '''
-    Returns 0 if sum of weights is 1.0
-    '''
-    return np.sum(weights) - 1
-# %% codecell
-# By convention of minimize function it should be a function that returns zero for conditions
-cons = ({'type':'eq','fun': check_sum})
-# %% codecell
-# 0-1 bounds for each weight
-bounds = ((0, 1), (0, 1), (0, 1), (0, 1))
-# %% codecell
-# Initial Guess (equal distribution)
-init_guess = [0.25,0.25,0.25,0.25]
-# %% codecell
-# Sequential Least SQuares Programming (SLSQP).
-opt_results = minimize(neg_sharpe,init_guess,method='SLSQP',bounds=bounds,constraints=cons)
-# %% codecell
-opt_results
-# %% codecell
-opt_results.x
-# %% codecell
-get_ret_vol_sr(opt_results.x)
-
-# %% Find Covariance and Correllation
 
 
-# %% calculate variance and volatility
+# %% Pyfolio Plot
+# TODO work out a better way to graph this.
+pyfo_info = efficient_frontier_models.pyfolio_eff_frontier(adj_close_df,cov_type="ledoit_wolf",returns = False,risk_free_rate=0.02)
+plt.figure(figsize=(10,10))
+Plotting.plot_efficient_frontier(pypfopt.cla.CLA(pyfo_info[1],pyfo_info[2],weight_bounds=(0, 1)), points=100, show_assets=True)
+
+plt.tight_layout()
+plt.show()
 
 
-# %% calculate diversifiable risk
-
-
-# %% calculate un-diversifiable risk
-
-
-# %% Recomend allocation adjustments
+# %%
--- Python_Core/stock_fetch.py
+++ Python_Core/stock_fetch.py
@@ -5,7 +5,11 @@
 # %% codecell
 import pandas as pd
 import pandas_datareader as wb
-class stock_fetch:    
+from datetime import datetime
+from pandas import ExcelWriter
+from pandas import ExcelFile
+
+class stock_utilities:    
     def yahoo_stock_fetch(tickers,start_date,end_date):
         """ function to pull multiple stocks into several dataframes from yahoo
 
@@ -25,15 +29,33 @@ class stock_fetch:
             d[tickers] = pd.DataFrame()
             d[tickers] = wb.DataReader(tickers, data_source='yahoo', start=(start_date), end=(end_date))
         return d
-# #Sample Required Input
-# tickers = 'PG,MSFT,F,GE'
-# start_date = '2012-01-01'
-# end_date = '2017-01-01'
-#
-# #call for stock information.
-# stock_df = stock_fetch.yahoo_stock_fetch(tickers, start_date, end_date)
-# #stock list
-# stock_df.keys()
-# #dataframes
-# for t in stock_df.keys():
-#     print(stock_df[t])
+
+    def write_to_excel(df,file_name="funds.xlsx"):
+        """Save dataframe(s) to excel
+
+        Args:
+            df ([DataFarme(s)]): DataFrame or Dictionary with multiple dataframes to send to excel
+            file_name ([str], optional): File name to save excel as. Defaults to funds.xlsx.
+        """
+        excel_name = pd.ExcelWriter(file_name, engine = 'xlsxwriter')
+        print('Saving:')
+        for i in df.keys():
+            print (i)
+            df[i].to_excel(excel_name,str(i))
+        excel_name.save()
+
+    def read_from_excel(file_path):
+        """Read excel sheet to a DataFrame.
+        !!Issues with large files!!
+
+        Args:
+            file_path ([str]): Path with a filename to read from. 
+            parse (bool, optional): Parse the DataFrame to use the dates as the index. Defaults to False.
+
+        Returns:
+            [DataFrame]: Excel DataFrame
+        """
+        all_sheets_df = pd.read_excel(file_path, sheet_name=None)
+        return all_sheets_df
+        
+
