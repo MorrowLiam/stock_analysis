300280257f03e697d6a6acce636e5a68eadb60d5
Liam  Morrow <l.morrow@perkinseastman.com>
2020-06-18 14:01:19 -0400

    update

--- Python_Core/stock_screener.py
+++ Python_Core/stock_screener.py
@@ -11,82 +11,33 @@ if __name__ == '__main__' and __package__ is None:
     sys.path.append(inputfilename2)
 
 # %% imports
-import numpy as np
 import pandas as pd
-from pandas_datareader import data as wb
 import matplotlib.pyplot as plt
 import seaborn as sns
 from datetime import datetime
-from pandas import ExcelWriter
-from pandas import ExcelFile
-
-from stock_fetch import stock_utilities as sf
-
+from stock_utilities import stock_utilities as su
 sns.set(style="darkgrid")
 
-
-import requests
-import time
-import os
-
-
-
-# %% pull data
-
+# %% fetch data
 tickers="T,AAPL,FB"
 start_date = pd.to_datetime('9/25/2015', utc=True)
 end_date = pd.to_datetime('6/16/2020', utc=True)
-stock_df = sf.yahoo_stock_fetch(tickers, start_date, end_date)
+#fetch yahoo stock information and save to a variable.
+stock_df = su.yahoo_stock_fetch(tickers, start_date, end_date)
+#Calculate a number of typical techincal indicators
+analysis_df = su.typ_tech_analysis_df(stock_df)
 
-# %% pull data
-#definition for typical markers and statistics
-# def setup_df(stock_df, RS_n_days):
-RS_n_days = 14
-analysis_df = {}
-for t in stock_df.keys():
-    analysis_df[t] = pd.DataFrame()
-    analysis_df[t]['Adj Close'] = (stock_df[t]['Adj Close'])
-    analysis_df[t]['Volume'] = (stock_df[t]['Volume'])
-    analysis_df[t]['Close Delta']  = (stock_df[t]['Adj Close'].diff(1))
-    analysis_df[t]['Simple Return %']  = (stock_df[t]['Adj Close'].pct_change(1))
-    analysis_df[t]['Total ROI %'] = ((stock_df[t]['Adj Close']-stock_df[t]['Adj Close'].iloc[0])/stock_df[t]['Adj Close'].iloc[0])*100
-    analysis_df[t]['Log Returns'] = np.log(stock_df[t]['Adj Close']/stock_df[t]['Adj Close'].shift(1))
-    
-    #calculate RSI
-    gains = analysis_df[t]['Close Delta'].mask(analysis_df[t]['Close Delta']<0,0)
-    losses = analysis_df[t]['Close Delta'].mask(analysis_df[t]['Close Delta']>0,0)
-    analysis_df[t][str(RS_n_days) + ' RS Average Gains'] = gains.ewm(com = RS_n_days -1, min_periods=RS_n_days).mean()
-    analysis_df[t][str(RS_n_days) + ' RS Average Losses'] = losses.ewm(com = RS_n_days -1, min_periods=RS_n_days).mean()
-    RS=abs(analysis_df[t][str(RS_n_days) + ' RS Average Gains']/analysis_df[t][str(RS_n_days) + ' RS Average Losses'])
-    analysis_df[t]['RSI'] = 100 - 100/ (1+RS)
-
-    #Calculate bolinger bands and rolling mean
-    analysis_df[t]['Close: 30 Day Mean'] = analysis_df[t]['Adj Close'].rolling(window=30).mean()
-    analysis_df[t]['Close: 50 Day Mean'] = analysis_df[t]['Adj Close'].rolling(window=50).mean()
-    analysis_df[t]['Close: 150 Day Mean'] = analysis_df[t]['Adj Close'].rolling(window=150).mean()
-    analysis_df[t]['Close: 200 Day Mean'] = analysis_df[t]['Adj Close'].rolling(window=200).mean()
-    analysis_df[t]['30 Day Upper Band'] = analysis_df[t]['Close: 30 Day Mean'] + 2*analysis_df[t]['Adj Close'].rolling(window=20).std()
-    analysis_df[t]['30 Day Lower Band'] = analysis_df[t]['Close: 30 Day Mean'] - 2*analysis_df[t]['Adj Close'].rolling(window=20).std()
-
-    #calc 52 week low/high
-    analysis_df[t]['52 Wk High'] = analysis_df[t]['Adj Close'].rolling(min_periods=1, window=252, center=False).max()
-    analysis_df[t]['52 Wk Low'] = analysis_df[t]['Adj Close'].rolling(min_periods=1, window=252, center=False).min()
-
-
-analysis_df['FB']
-
-# %% pull data
+# %% Plot data
+#test plot of RSI,Close,and BOlinger Bands
 fig, ax = plt.subplots(figsize=(16,10))
 analysis_df['FB']['RSI'].plot()
 analysis_df['FB']['Adj Close'].plot(figsize=(16,6))
 analysis_df['FB']['Close: 30 Day Mean'].plot(figsize=(16,6))
-analysis_df['FB']['Upper Band'].plot(figsize=(16,6))
-analysis_df['FB']['Lower Band'].plot(figsize=(16,6))
+analysis_df['FB']['30 Day Upper Band'].plot(figsize=(16,6))
+analysis_df['FB']['30 Day Lower Band'].plot(figsize=(16,6))
 ax.legend()
 plt.tight_layout()
 plt.show()
-# %% pull data
-
 
 # %% pull data
 
@@ -95,12 +46,12 @@ plt.show()
 # for stock in stocklist:
 #     n += 1
 #     time.sleep(1)
-    
+
 #     print ("\npulling {} with index {}".format(stock, n))
 #     # rsi value
 #     start_date = datetime.datetime.now() - datetime.timedelta(days=365)
 #     end_date = datetime.date.today()
-    
+
 #     df = pdr.get_data_yahoo(stock, start=start_date, end=end_date)
 #     df = df.reset_index()
 #     df['Date'] = pd.to_datetime(df.Date)
@@ -123,7 +74,7 @@ plt.show()
 #     rsi.columns = ['Value']
 #     rsi_list = rsi.Value.to_list()
 #     RS_Rating = rsi['Value'].mean()
-    
+
 # try:
 #         sma = [50, 150, 200]
 #         for x in sma:
@@ -135,7 +86,7 @@ plt.show()
 #         moving_average_200 = df["SMA_200"][-1]
 #         low_of_52week = min(df["Adj Close"][-260:])
 #         high_of_52week = max(df["Adj Close"][-260:])
-        
+
 #         try:
 #             moving_average_200_20 = df["SMA_200"][-20]
 
@@ -188,11 +139,11 @@ plt.show()
 #         if(condition_1 and condition_2 and condition_3 and condition_4 and condition_5 and condition_6 and condition_7 and condition_8):
 #             final.append(stock)
 #             index.append(n)
-            
+
 #             dataframe = pd.DataFrame(list(zip(final, index)), columns =['Company', 'Index'])
-            
+
 #             dataframe.to_csv('stocks.csv')
-            
+
 #             exportList = exportList.append({'Stock': stock, "RS_Rating": RS_Rating, "50 Day MA": moving_average_50, "150 Day Ma": moving_average_150, "200 Day MA": moving_average_200, "52 Week Low": low_of_52week, "52 week High": high_of_52week}, ignore_index=True)
 #             print (stock + " made the requirements")
 #     except Exception as e:
